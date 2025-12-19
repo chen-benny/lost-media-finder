f0022235a648f7db807b7eea53cee887a0d106c4
chen-benny <chenbenny31@gmail.com>
2025-12-18 23:21:53 -0800

    add japanese character and date filter

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"regexp"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -18,13 +19,36 @@ const (
 
 var (
 	visited    = make(map[string]bool)
-	queue      = []string{"https://www.vidlii.com"}
+	queue      = []string{"https://www.vidlii.com"] //, "https://www.vidlii.com/user/rinkomania"}
 	videoCount = 0
-	pageCount  = 0
+	// pageCount  = 0
 	dateRe     = regexp.MustCompile(`[A-Z][a-z]{2} \d{1,2}, \d{4}`)
+	cutoffDate = time.Date(2021, 12, 31, 23, 59, 59, 0, time.UTC)
 )
 
+type Video struct {
+	URL   string
+	Title string
+	Date  time.Time
+}
+
+func (v Video) Match() bool {
+	// date before 2021-12-31
+	if v.Date.IsZero() || !v.Date.Before(cutoffDate) {
+		return false
+	}
+	// title contains Japanese characters
+	for _, ch := range v.Title {
+		if unicode.In(ch, unicode.Hiragana, unicode.Katakana, unicode.Han) {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
+	var targets []Video
+
 	for len(queue) > 0 && videoCount < maxVideos {
 		url := queue[0]
 		queue = queue[1:]
@@ -46,15 +70,22 @@ func main() {
 			continue
 		}
 
-		pageCount++
-		log.Printf("[%d] %s", pageCount, url)
+		// pageCount++
+		// log.Printf("[%d] %s", pageCount, url)
 
 		// if video page
 		if strings.Contains(url, "/watch?v=") {
 			videoCount++
 			title := strings.TrimSuffix(doc.Find("title").Text(), " - VidLii")
-			date := dateRe.FindString(doc.Text())
+			dateStr := dateRe.FindString(doc.Text())
+			date, _ := time.Parse("Jan 2, 2006", dateStr)
+
+			v := Video{URL: url, Title: title, Date: date}
 			log.Printf("[%d] %s | %s | %s", videoCount, title, date, url)
+			if v.Match() {
+				targets = append(targets, v)
+				log.Printf("Found One Match: %s", url)
+			}
 		}
 
 		// append new URLs
@@ -70,4 +101,8 @@ func main() {
 	}
 
 	fmt.Printf("\nDone, visited %d videos\n", videoCount)
+
+	for i, video := range targets {
+		fmt.Println(i+1, video.URL)
+	}
 }
